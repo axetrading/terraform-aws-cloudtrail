779eab34d1cb31234330b9cf980ec4da2e14ad77
Ciprian Ursu <devops@ciprianursu.com>
2024-05-02 15:54:09 +0300

    fix: outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "s3_bucket_id" {
-  value       = aws_s3_bucket.cloudtrail_bucket[0].id
+  value       = var.enabled && var.create_cloudtrail_bucket ? aws_s3_bucket.cloudtrail_bucket[0].id : null
   description = "The ID of the S3 bucket used for CloudTrail logs"
 }
 
 output "cloudtrail_arn" {
-  value       = aws_cloudtrail.trail[0].arn
+  value       = var.enabled ? aws_cloudtrail.trail[0].arn : null
   description = "The ARN of the CloudTrail"
 }
